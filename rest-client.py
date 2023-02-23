a1922df1f247db2981f89a17188b7793c27b7e23
Edgar Torres <edgar.torres@rub.de>
2023-02-23 21:00:27 +0100

    added rest api

---
+++ rest-client.py
@@ -0,0 +1,8 @@
+import requests
+
+if __name__=="__main__":
+    URL = "https://127.0.0.1:8080/echo"
+    POST = "Hello"
+
+    r = requests.post(url=URL, data=POST, verify=False)
+    print(r.text)
